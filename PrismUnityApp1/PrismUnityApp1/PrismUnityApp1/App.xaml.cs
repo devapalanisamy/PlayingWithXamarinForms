f3b0f3106ee8702ef665fbf01569d61511fdd686
SOFTWARE\deva.palanisamy <dev.palanisamy@gmail.com>
2016-12-04 09:48:47 +0000

    app level resourcedictionary can be applied to any page or any view

--- PrismUnityApp1/PrismUnityApp1/PrismUnityApp1/App.xaml.cs
+++ PrismUnityApp1/PrismUnityApp1/PrismUnityApp1/App.xaml.cs
@@ -6,11 +6,38 @@ namespace PrismUnityApp1
 {
     public partial class App : PrismApplication
     {
-        public App(IPlatformInitializer initializer = null) : base(initializer) { }
+        public App(IPlatformInitializer initializer = null) : base(initializer)
+        {
+
+        }
 
         protected override void OnInitialized()
         {
             InitializeComponent();
+
+            var labelRedStyle = new Style(typeof(Label))
+            {
+                Setters = {
+                new Setter { Property = Label.TextColorProperty, Value = Color.Red  }
+            }
+            };
+            var labelGreenStyle = new Style(typeof(Label))
+            {
+                Setters = {
+                new Setter { Property = Label.TextColorProperty, Value = Color.Green }
+            }
+            };
+            var labelBlueStyle = new Style(typeof(Label))
+            {
+                Setters = {
+                new Setter { Property = Label.TextColorProperty, Value = Color.Blue }
+            }
+            };
+
+            Resources = new ResourceDictionary();
+            Resources.Add("labelRedStyle", labelRedStyle);
+            Resources.Add("labelGreenStyle", labelGreenStyle);
+            Resources.Add("labelBlueStyle", labelBlueStyle);
 
             NavigationService.NavigateAsync("RootPage/MainPage?title=Hello%20from%20Xamarin.Forms");
         }
--- PrismUnityApp1/PrismUnityApp1/PrismUnityApp1/Views/MainPage.xaml.cs
+++ PrismUnityApp1/PrismUnityApp1/PrismUnityApp1/Views/MainPage.xaml.cs
@@ -7,6 +7,33 @@ namespace PrismUnityApp1.Views
     {
         public MainPage()
         {
+            /***
+            var labelRedStyle = new Style(typeof(Label))
+            {
+                Setters = {
+                new Setter { Property = Label.TextColorProperty, Value = Color.Red  }
+            }
+            };
+            var labelGreenStyle = new Style(typeof(Label))
+            {
+                Setters = {
+                new Setter { Property = Label.TextColorProperty, Value = Color.Green }
+            }
+            };
+            var labelBlueStyle = new Style(typeof(Label))
+            {
+                Setters = {
+                new Setter { Property = Label.TextColorProperty, Value = Color.Blue }
+            }
+            };
+
+            Resources = new ResourceDictionary();
+            Resources.Add("labelRedStyle", labelRedStyle);
+            Resources.Add("labelGreenStyle", labelGreenStyle);
+            Resources.Add("labelBlueStyle", labelBlueStyle);
+            ***/
+            //Resources = Application.Current.Resources;
+
             InitializeComponent();
         }
 }
